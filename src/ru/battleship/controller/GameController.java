472375cf12de8bcb3e6166fb15eeed6631fffac4
Pavel Uspenskij <uspenskiydj@gmail.com>
2019-11-05 23:42:35 +0400

    feature(GameController.java) add an event to AI Board

--- src/ru/battleship/controller/GameController.java
+++ src/ru/battleship/controller/GameController.java
@@ -3,8 +3,14 @@ package ru.battleship.controller;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import ru.battleship.objects.Game;
+import ru.battleship.objects.Ship;
 
 public class GameController {
     
@@ -28,5 +34,24 @@ public class GameController {
     
     public void initializeAIBoard(GridPane pnGridBox) {
         pnGridAI.getChildren().addAll(pnGridBox.getChildren());
+    }
+    
+    public void pnGridAIMouseReleasedAction(MouseEvent e) {
+        if (e.getButton() == MouseButton.SECONDARY) {
+            return;
+        }
+        if (e.getTarget() instanceof Ship) {
+            Ship ship = (Ship) e.getTarget();
+            ship.setOpacity(1);
+            ship.setImage(new Image(getClass().getResourceAsStream("/ru/battleship/icons/wreck.png")));
+        }
+        else if (e.getTarget() instanceof Pane) {
+            Pane pane = (Pane) e.getTarget();
+            if (pane.getChildren().isEmpty()) {
+                pane.getChildren().add(new ImageView(
+                        new Image(getClass().getResourceAsStream("/ru/battleship/icons/shot.png"))
+                ));
+            }
+        }
     }
 }
